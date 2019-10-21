8fb1fddb84f400093fc51faa8e926352e33aec17
Evan <evan@users.noreply.github.com>
2019-10-21 17:40:57 +1000

    new point cloud

---
+++ pointclouds/July_2019/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=utm +zone=56 +south +datum=WGS84 +units=m +no_defs",
+    "points": 36578093,
+    "boundingBox": {
+        "lx": 559545.3200000001,
+        "ly": 6831307.4,
+        "lz": -8.76,
+        "ux": 561703.47,
+        "uy": 6833465.550000001,
+        "uz": 2149.3899999999066
+    },
+    "tightBoundingBox": {
+        "lx": 559545.3200000001,
+        "ly": 6831307.41,
+        "lz": -8.77,
+        "ux": 561703.48,
+        "uy": 6832173.25,
+        "uz": 42.550000000000007
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 18.690126419067384,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
